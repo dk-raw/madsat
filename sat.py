a87762d1fb743ec672b4c8abc87f3881c0f709d4
damian <ddkatsios@protonmail.com>
2024-07-24 16:31:35 +0300

    event logging and tweets

--- sat.py
+++ sat.py
@@ -4,10 +4,16 @@ import numpy as np
 import time as tm
 import uuid 
 import csv
+import twitter
 
 last_tle_update = tm.time()
+last_mag_update = tm.time()
 DEG_MARGIN = 2
+EVENT_DURATION_THRESHOLD = 5*60
+last_event_time = {}
 
+global satellite
+global tles
 satellites = []
 tles = []
 
@@ -34,7 +40,7 @@ try:
           res = requests.get(url)
           tle = res.text.splitlines()
           tles.append(tle)
-     print(f"{len(tles)} TLE data fetched successfully.")
+     print(f"{len(tles)} TLEs fetched successfully.")
 except Exception as e:
      print(e)
 
@@ -50,9 +56,9 @@ def haversine(lat1, lon1, lat2, lon2):
         c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
         return round(R * c,1)
 
-def saveEvent(time, observatory, sat):
+def saveEvent(time, observatory, sat, id):
      eventId = uuid.uuid4()
-     eventData = f'{eventId},{time},{observatory["IAGA"]},{observatory["Name"]},{observatory["Lat"]},{observatory["Lon"]},{sat["ID"]},{sat["Name"]},False\n'
+     eventData = f'{eventId},{time},{observatory["IAGA"]},{observatory["Name"]},{observatory["Lat"]},{observatory["Lon"]},{sat["ID"]},{sat["Name"]},False,{id}\n'
      try:
           with open("events.txt", 'a') as file:
                file.write(eventData)
@@ -62,19 +68,21 @@ def saveEvent(time, observatory, sat):
 
 def updateTLE():
      try:
-          tles=[]
+          global tles
+          updated_tles = []
           for sat in satellites:
                url = f"https://celestrak.com/NORAD/elements/gp.php?CATNR={sat}"
                res = requests.get(url)
                tle = res.text.splitlines()
-               tles.append(tle)
-          print(f"{len(tles)} TLE data updated successfully.")
+               updated_tles.append(tle)
+          print(f"{len(updated_tles)} TLEs updated successfully.")
+          tles = updated_tles
      except Exception as e:
-          print(e)
+          print(f"TLEs were not updated due to an error. Using previous TLEs...\n{e}")
 
 while True:
     current_time = tm.time()
-    if current_time - last_tle_update >= 30: #time is seconds
+    if current_time - last_tle_update >= 3600: #time is seconds
          updateTLE()
          last_tle_update = current_time
     
@@ -85,29 +93,33 @@ while True:
      satellite = EarthSatellite(tle[1], tle[2], tle[0], ts)
      geocentric = satellite.at(time)
      subpoint = geocentric.subpoint()
-    
      sat_lat, sat_lon = subpoint.latitude.degrees, subpoint.longitude.degrees
 
-     #print(f"{satellite.name} is at {sat_lat}, {sat_lon}")
+     # print(f"{satellite.name} is at {sat_lat}, {sat_lon}")
     
      for iaga, name, lat, lon in parsed_observatories:
         distance = haversine(lat, lon, sat_lat, sat_lon)
         print(f"Distance from {iaga} to {satellite.name} satellite: {distance} km")
         
         if distance <= DEG_MARGIN * 111:  # Convert degrees to approximate km (1 degree ~ 111 km)
-            obs = {
-                "IAGA": iaga,
-                "Name": name,
-                "Lat": lat,
-                "Lon": lon
-            }
-            sat = {
-                "ID": satellite.model.satnum,
-                "Name": satellite.name
-            }
-            saveEvent(current_time, obs, sat)
             print(f"Satellite {satellite.name} is within 2 degrees of the {iaga} station")
+            key = (satellite.model.satnum, iaga)
+            if key not in last_event_time or current_time - last_event_time[key] >= EVENT_DURATION_THRESHOLD:
+               obs = {
+                    "IAGA": iaga,
+                    "Name": name,
+                    "Lat": lat,
+                    "Lon": lon
+               }
+               sat = {
+                    "ID": satellite.model.satnum,
+                    "Name": satellite.name
+               }
+               tweetId = twitter.tweet(f"🔔🛰️ Satellite {satellite.name} ({satellite.model.satnum}) is now close to {name} ({iaga}) observatory.")
+               saveEvent(current_time, obs, sat, tweetId)
+               last_event_time[key] = current_time
+     
 
         
     print("================================================")
-    tm.sleep(4) 
+    tm.sleep(5) 
--- twitter.py
+++ twitter.py
@@ -1,19 +1,30 @@
 import tweepy
+import os
+from dotenv import load_dotenv
 
-# Replace these values with your credentials
-API_KEY = '8bmtO48h9rSrbniNFsO4Ni8bF'
-API_SECRET_KEY = '1S6JpE1D1gtYcDaAWKOEe5mw4mcOY0VPEZKRaAGJJ3VF0tx7op'
-ACCESS_TOKEN = '1324811255018885120-gfdAqYRMNmwvHXFedWU1TIo2GOa3TJ'
-ACCESS_TOKEN_SECRET = 'tdscSDTfSQNobj40eyCMxejCtnbOLvMNlbZjkaWJruc4O'
+load_dotenv()
 
-# Authenticate to Twitter
-auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+consumer_key = os.getenv('consumer_key')
+consumer_secret = os.getenv('consumer_secret')
+access_token = os.getenv('access_token')
+access_token_secret = os.getenv('access_token_secret')
 
-# Post a tweet
-tweet = "test test test"
-try:
-    api.update_status(status=tweet)
-    print("Tweet posted successfully!")
-except tweepy.TweepError as e:
-    print(f"Error: {e.reason}")
+client = tweepy.Client(
+    consumer_key=consumer_key, consumer_secret=consumer_secret,
+    access_token=access_token, access_token_secret=access_token_secret
+)
+
+def tweet(content):
+    response = client.create_tweet(
+        text=content
+    )
+    print(f"https://twitter.com/user/status/{response.data['id']}")
+    return response.data['id']
+
+def reply(content, id):
+    response = client.create_tweet(
+        text=content,
+        in_reply_to_tweet_id=id
+    )
+    print(f"https://twitter.com/user/status/{response.data['id']}")
+    return response.data['id']
