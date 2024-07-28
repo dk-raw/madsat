1c4813c35e033f1a2270e3f8e2452dbf91e2018c
damian <ddkatsios@protonmail.com>
2024-07-28 21:56:53 +0300

    update shell code to include virtual environment

--- update.sh
+++ update.sh
@@ -2,8 +2,14 @@
 cd /home/nikolakis/madsat
 echo "Pulling latest changes from GitHub..."
 git pull origin main
+
+echo "Creating virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
+
 echo "Installing dependencies..."
-pip3 install -r requirements.txt
+pip install -r requirements.txt
+
 echo "Restarting the application..."
 sudo systemctl restart madsat.service
 echo "Update complete!"
